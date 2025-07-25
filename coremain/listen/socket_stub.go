PuerNya/mosdns

feat: enlarge receive/send buffer && set reuse_addr/reuse_port

coremain/socket_stub.go
@@ -1,0 +1,11 @@
+//go:build !darwin && !dragonfly && !freebsd && !linux && !netbsd && !openbsd && !solaris && !windows
+
+package coremain
+
+import (
+	"net"
+)
+
+func createListenConfig() net.ListenConfig {
+	return net.ListenConfig{}
+}

coremain/socket_unix.go
@@ -1,0 +1,38 @@
+//go:build darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
+
+package coremain
+
+import (
+	"net"
+	"syscall"
+)
+
+func createListenConfig() net.ListenConfig {
+	return net.ListenConfig{
+		Control: func(network, address string, c syscall.RawConn) error {
+			var e error
+			err := c.Control(func(fd uintptr) {
+				e = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+				if e != nil {
+					return
+				}
+				e = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_REUSEPORT, 1)
+				if e != nil {
+					return
+				}
+				e = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF, 64*1024)
+				if e != nil {
+					return
+				}
+				e = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDBUF, 64*1024)
+			})
+			if err != nil {
+				return err
+			}
+			if e != nil {
+				return e
+			}
+			return nil
+		},
+	}
+}

coremain/socket_windows.go
@@ -1,0 +1,34 @@
+//go:build windows
+
+package coremain
+
+import (
+	"net"
+	"syscall"
+)
+
+func createListenConfig() net.ListenConfig {
+	return net.ListenConfig{
+		Control: func(network, address string, c syscall.RawConn) error {
+			var e error
+			err := c.Control(func(fd uintptr) {
+				e = syscall.SetsockoptInt(syscall.Handle(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+				if e != nil {
+					return
+				}
+				e = syscall.SetsockoptInt(syscall.Handle(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF, 64*1024)
+				if e != nil {
+					return
+				}
+				e = syscall.SetsockoptInt(syscall.Handle(fd), syscall.SOL_SOCKET, syscall.SO_SNDBUF, 64*1024)
+			})
+			if err != nil {
+				return err
+			}
+			if e != nil {
+				return e
+			}
+			return nil
+		},
+	}
+}

pmkol/mosdns-x

coremain/listen/socket_stub.go
@@ -1,0 +1,11 @@
+//go:build !darwin && !dragonfly && !freebsd && !linux && !netbsd && !openbsd && !solaris && !windows
+
+package listen
+
+import (
+	"net"
+)
+
+func CreateListenConfig() net.ListenConfig {
+	return net.ListenConfig{}
+}

coremain/listen/socket_unix.go
@@ -1,0 +1,40 @@
+//go:build darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris
+
+package listen
+
+import (
+	"net"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+func CreateListenConfig() net.ListenConfig {
+	return net.ListenConfig{
+		Control: func(network, address string, c syscall.RawConn) error {
+			var e error
+			err := c.Control(func(fd uintptr) {
+				e = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+				if e != nil {
+					return
+				}
+				e = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+				if e != nil {
+					return
+				}
+				e = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_RCVBUF, 64*1024)
+				if e != nil {
+					return
+				}
+				e = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_SNDBUF, 64*1024)
+			})
+			if err != nil {
+				return err
+			}
+			if e != nil {
+				return e
+			}
+			return nil
+		},
+	}
+}

coremain/listen/socket_windows.go
@@ -1,0 +1,34 @@
+//go:build windows
+
+package listen
+
+import (
+	"net"
+	"syscall"
+)
+
+func CreateListenConfig() net.ListenConfig {
+	return net.ListenConfig{
+		Control: func(network, address string, c syscall.RawConn) error {
+			var e error
+			err := c.Control(func(fd uintptr) {
+				e = syscall.SetsockoptInt(syscall.Handle(fd), syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+				if e != nil {
+					return
+				}
+				e = syscall.SetsockoptInt(syscall.Handle(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF, 64*1024)
+				if e != nil {
+					return
+				}
+				e = syscall.SetsockoptInt(syscall.Handle(fd), syscall.SOL_SOCKET, syscall.SO_SNDBUF, 64*1024)
+			})
+			if err != nil {
+				return err
+			}
+			if e != nil {
+				return e
+			}
+			return nil
+		},
+	}
+}