f56bb1d0ce5b91ab365e5605391ecc9258a4d5f6
pmkol <pmkol@users.noreply.github.com>
2026-01-20 16:52:20 +0800

    fix: correctly handle CNAME flattening after upstream execution for plugin _no_cname

--- plugin/executable/no_cname/no_cname.go
+++ plugin/executable/no_cname/no_cname.go
@@ -21,10 +21,12 @@ package no_cname
 
 import (
 	"context"
+
+	"github.com/miekg/dns"
+
 	"github.com/pmkol/mosdns-x/coremain"
 	"github.com/pmkol/mosdns-x/pkg/executable_seq"
 	"github.com/pmkol/mosdns-x/pkg/query_context"
-	"github.com/miekg/dns"
 )
 
 const (
@@ -43,24 +45,51 @@ type noCNAME struct {
 	*coremain.BP
 }
 
-func (t *noCNAME) Exec(ctx context.Context, qCtx *query_context.Context, next executable_seq.ExecutableChainNode) error {
+func (t *noCNAME) Exec(
+	ctx context.Context,
+	qCtx *query_context.Context,
+	next executable_seq.ExecutableChainNode,
+) error {
+
 	if err := executable_seq.ExecChainNode(ctx, qCtx, next); err != nil {
 		return err
 	}
 
 	r := qCtx.R()
-	if r == nil {
+	if r == nil || len(r.Answer) == 0 {
 		return nil
 	}
 
-	rr := r.Answer[:0]
-	for _, ar := range r.Answer {
-		if ar.Header().Rrtype == dns.TypeCNAME {
+	q := qCtx.Q()
+	if q == nil || len(q.Question) == 0 {
+		return nil
+	}
+	qName := q.Question[0].Name
+
+	hasIP := false
+	for _, rr := range r.Answer {
+		switch rr.Header().Rrtype {
+		case dns.TypeDNAME:
+			return nil
+		case dns.TypeA, dns.TypeAAAA:
+			hasIP = true
+		}
+	}
+
+	if !hasIP {
+		return nil
+	}
+
+	filtered := r.Answer[:0]
+
+	for _, rr := range r.Answer {
+		if rr.Header().Rrtype == dns.TypeCNAME {
 			continue
 		}
-		rr = append(rr, ar)
+		rr.Header().Name = qName
+		filtered = append(filtered, rr)
 	}
-	r.Answer = rr
 
+	r.Answer = filtered
 	return nil
 }
